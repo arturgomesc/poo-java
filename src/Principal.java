752f1f8af3441033f388313e8d6fd7c9c1112d49
arturgomesc <arturgomesc@hotmail.com>
2023-04-24 17:11:31 -0300

    adicionando novos codigos

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,29 @@
+import br.com.screenmatch.modelos.Serie;
 import br.com.screenmatch.modelos.filme;
 
 public class Principal {
     public static void main(String[] args) {
         filme meuFilme = new filme();
+        meuFilme.setDiretor("Copolla");
         meuFilme.setNome("The Godfather");
         meuFilme.setAnoDeLancamento(1972);
         meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(9);
-        System.out.println(meuFilme.getTotalDeAlavaliacoes());
-        System.out.println(meuFilme.getTotalDeAlavaliacoes());
-        System.out.println(meuFilme.pegaMedia());
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAlavaliacoes());
+        System.out.println("Média de notas: " + meuFilme.pegaMedia());
+
+        Serie theOffice = new Serie();
+        theOffice.setNome("The Office");
+        theOffice.setAnoDeLancamento(2005);
+        theOffice.exibeFichaTecnica();
+        theOffice.setTemporadas(9);
+        theOffice.setAtiva(false);
+        theOffice.setEpisodiosPorTemporada(28);
+        theOffice.setMinutosPorEpisodio(22);
     }
 }
--- src/br/com/screenmatch/modelos/filme.java
+++ src/br/com/screenmatch/modelos/filme.java
@@ -1,60 +1,13 @@
 package br.com.screenmatch.modelos;
 
-public class filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDeAvaliacoes;
-    private int totalDeAlavaliacoes;
-    private int duracaoEmMinutos;
+public class filme extends Titulo {
+    private String diretor;
 
-    public String getNome() {
-        return nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public int getTotalDeAlavaliacoes() {
-        return totalDeAlavaliacoes;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void exibeFichaTecnica() {
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota) {
-        somaDeAvaliacoes += nota;
-        totalDeAlavaliacoes++;
-    }
-
-    public double pegaMedia() {
-        return somaDeAvaliacoes / totalDeAlavaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
